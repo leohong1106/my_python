72255ec627ab49c30bb8a1b571106206531319c0
leohong1106 <52034865+leohong1106@users.noreply.github.com>
2019-10-03 22:10:16 +0900

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,13 @@
+#191003 slicing
+tomorrow = "ethereum conference"
+print(tomorrow)
+print(tomorrow[0:8])
+print(tomorrow[0:9])
+print(tomorrow[9:12])
+
+#string.split()
+print(tomorrow.split())
+과일 = '거봉 수박 포도 복숭아 망고 딸기 배 참외'
+print(과일)
+과일나누기 = 과일.split()
+print(과일나누기)
