db6b274f97930677f8abf1714fe57ebf9f5f984e
leohong1106 <52034865+leohong1106@users.noreply.github.com>
2019-10-09 23:01:24 +0900

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,27 @@
+#Operator
+#연산자는 특정한 연산을 하는 것.
+
+count = 0
+print(count)
+count = count + 1
+print(count)
+count = count + 1
+print(count)
+count += 1
+print(count)
+count -= 1
+print(count)
+
+#Arithmetic(산술 연산자)
+print(
+    3 * 4
+)
+# ** (제곱)
+# // (나누었을 때 나눠지는 몫)
+# % (나머지를 구하는 연산자)
+
+print(3**2)
+print(4**3)
+print(7/3)
+print(7//3)
+print(7%3)
