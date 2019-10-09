35f123d3983689752af40b9b866040ed0145ba89
leohong1106 <52034865+leohong1106@users.noreply.github.com>
2019-10-09 23:00:57 +0900

    Add files via upload

---
+++ range.py
@@ -0,0 +1,22 @@
+print(range(3))
+print(
+    type(range(3))
+)
+
+for n in [0, 1, 2]:
+    print(n)
+
+for n in range(0,3):
+    print(n)
+
+for n in range(0,101):
+    print(n)
+
+print(range(4,7))
+
+
+for i in range(1,10):
+    print(i)
+
+for i in range(1,10):
+    print('{}x{}={}'.format(2,i,2*i))
